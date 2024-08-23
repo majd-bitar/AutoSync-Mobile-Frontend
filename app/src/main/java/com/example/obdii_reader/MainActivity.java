3078bfb19c001d99368a23ab8becded350c2d984
Majd <majdbitar2003@gmail.com>
2024-08-23 14:40:25 +0300

    Created MainActivity to test ObdReaderService provided by the library

--- app/src/main/java/com/example/obdii_reader/MainActivity.java
+++ app/src/main/java/com/example/obdii_reader/MainActivity.java
@@ -79,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
